549e0643ac7f8e914e90a6e4387a7871e6694a90
James Rivett-Carnac & Jason Milkins <pair+jrc+jm@pivotal.io>
2016-07-13 04:37:20 +0000

    Slabs of kawaii chocolate

--- reviews/integration_test.go
+++ reviews/integration_test.go
@@ -1,6 +1,8 @@
 package reviews_test
 
 import (
+	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -11,15 +13,15 @@ import (
 )
 
 func TestIntegration(t *testing.T) {
+	os.Remove("reviews_test.db")
 	go func() {
 		service := reviews.NewService("reviews_test.db")
 		service.Run()
 	}()
+	client := reviews.NewClient()
 
-	time.Sleep(5 * time.Second)
+	time.Sleep(5000 * time.Millisecond)
 	t.Run("storage=file", func(t *testing.T) {
-		client := reviews.NewClient()
-
 		reviewRequest := &proto.ReviewRequest{
 			Reviewer: "James",
 			Name:     "Hershy's Dark Cardboard",
@@ -31,6 +33,26 @@ func TestIntegration(t *testing.T) {
 
 		if err != nil {
 			t.Errorf("Expected error to be nil, was  '%v'", err)
+		}
+
+	})
+
+	t.Run("storage=file2", func(t *testing.T) {
+		expected := &proto.ReviewList{
+			Count: 1,
+			Reviews: []*proto.ReviewDetails{&proto.ReviewDetails{
+				Reviewer: "James",
+				Name:     "Hershy's Dark Cardboard",
+				Review:   "I ate the wrapper as well, and it tasted better than the chocolate",
+				Rating:   -5,
+			}}}
+		allReviews, err := client.AllReviews(context.TODO(), &proto.Empty{})
+
+		if err != nil {
+			t.Errorf("Expected error to be nil, was  '%v'", err)
+		}
+		if !reflect.DeepEqual(expected, allReviews) {
+			t.Errorf("Expected allReviews to be '%v', was  '%v'", expected, allReviews)
 		}
 
 	})
